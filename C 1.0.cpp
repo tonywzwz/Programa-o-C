15112974f602d60b322e867dcb4f571641f962ee
tonywzwz <161043062+tonywzwz@users.noreply.github.com>
2024-03-15 11:32:03 -0300

    Add files via upload

---
+++ C 1.0.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// std - > standard
+// i - > input
+// 0 - > outout 
+
+//Função principal.
+int main (){
+
+    //comando para mostrar mo terminal.
+    printf("Ola, mundo! Ola, Anthony!");
+   
+   //comando para pressionar uma tecla
+   // antes de finalizar p programa. 
+    getchar();
+    
+    
+    //Comando para retornar o código 0. 
+    //Código usado para informar que o 
+    //programa finalizou correntemente.
+    return 0; 
+}
+

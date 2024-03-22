ec4d2028d1c8f768c508705f6d10d7bdf25ce793
tonywzwz <161043062+tonywzwz@users.noreply.github.com>
2024-03-22 10:25:39 -0300

    Update and rename tiv 4.cpp to salario .cpp

--- salario .cpp
+++ salario .cpp
@@ -7,7 +7,7 @@ setlocale(LC_ALL, "");
    
 float salario, quantidadeSalarios, salarioMinino = 1412.00;
 
-printf("Digite o valor do sal·rio: ");
+printf("Digite o valor do sal√°rio: ");
 scanf ("%f", &salario);
 
 quantidadeSalarios = salario / salarioMinino;
@@ -17,8 +17,8 @@ system("cls || clear"); // Limpa o terminal no Windows e Linux.
 printf("'\n=== Exibindo resultados ===\n");
 
 
-printf("Sal·rio informado: R$ %.2f \n", salario);
-printf("Quantidade de sal·rios: %.1f \n", quantidadeSalarios);
+printf("Sal√°rio informado: R$ %.2f \n", salario);
+printf("Quantidade de sal√°rios: %.1f \n", quantidadeSalarios);
 
 return 0;
 
